2a8c85c38c178b41ee2872dd42d873e92416aa74
trucy <vvictoire@proton.me>
2024-11-08 21:32:29 +0100

    lint the code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import discord
-import json
+"""Discord bot for the Future Invaders card game"""
+
 import os
 import requests
+import discord
 from discord import guild_only
 from dotenv import load_dotenv
 from markdownify import markdownify
@@ -9,64 +10,72 @@ from requests.exceptions import HTTPError
 
 
 load_dotenv() # load all the variables from the env file
-LANGUAGES = ['en', 'fr']
+API_LANGUAGES = ['en', 'fr']
 bot = discord.Bot()
 
 def process_response(response: str, language: str):
+    """Returns the result of a card query, nicely formatted"""
     if response['cards'] is None:
         return "No cards found :("
-    else:
-        return embed_card(response['cards'][0], language)
+    return embed_card(response['cards'][0], language)
 
 def embed_card(card: dict, language: str):
+    """Formats an embed to display a card"""
     embed = discord.Embed(
         title=f"{card['name'][language]}",
         description=f"{card['faction']['name'][language]} {card['type']['name'][language]}",
-        color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
+        # TODO: use faction color
+        color=discord.Colour.blurple(),
     )
     if card['cost'] == "":
         card['cost'] = "None"
     if card['income'] == "":
         card['income'] = "None"
     card_body = markdownify(card['body'][language])
-    embed.add_field(name="Resources", value=f"**Cost:** {card['cost']} **Income:** {card['income']}", inline=True)
-    embed.add_field(name="Combat", value=f"**Weapons:** {card['weapons']} **Durability:** {card['durability']}", inline=True)
+    rarity = card['rarity']['name'][language]
+    release = card['release']['name'][language]
+    embed.add_field(name="Resources",
+                    value=f"**Cost:** {card['cost']} **Income:** {card['income']}",
+                    inline=True)
+    embed.add_field(name="Combat",
+                    value=f"**Weapons:** {card['weapons']} **Durability:** {card['durability']}",
+                    inline=True)
     embed.add_field(name="Effect", value=f"{card_body}",inline=False)
     embed.add_field(name="Database link", value=f"{card['url']}")
-    embed.set_footer(text=f"Rarity: {card['rarity']['name'][language]} - Release: {card['release']['name'][language]}")
+    embed.set_footer(text=f"Rarity: {rarity} - Release: {release}")
     embed.set_thumbnail(url=f"{card['images'][language]['path']}")
 
     return embed
 
-async def languages(context: discord.ApplicationContext):
-    return LANGUAGES
+async def languages(_: discord.ApplicationContext):
+    """Returns the languages supported by the API"""
+    return API_LANGUAGES
 
 @bot.event
 async def on_ready():
+    """Actions to do when the bot is online"""
     print(f"{bot.user} is ready and online!")
 
-@bot.slash_command(name="hello", description="Say hello to the bot")
-async def hello(context: discord.ApplicationContext):
-    await context.respond("Hey!")
-
-#async def cardsearch(context: discord.ApplicationContext, query: discord.SlashCommandOptionType.string, languages: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(languages))):
 @bot.slash_command(name="cardsearch", description="Search for a card")
 @guild_only()
 async def cardsearch(
     context: discord.ApplicationContext,
     query: discord.Option(str),
-    languages: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(languages), default=LANGUAGES[0])
+    language: discord.Option(str,
+                             autocomplete=discord.utils.basic_autocomplete(languages),
+                             default=API_LANGUAGES[0])
     ):
+    """Searches for a card given its language"""
     try:
         url = os.getenv('API_ENDPOINT')
-        response = requests.get(f"{url}/cards", params={'name': query})
+        response = requests.get(f"{url}/cards", params={'name': query}, timeout=10)
         response.raise_for_status()
-        answer = process_response(response.json(), languages)
-        await context.respond(embed=answer)
+        answer = process_response(response.json(), language)
+        if answer is str:
+            await context.respond(content=answer)
+        if answer is discord.Embed:
+            await context.respond(embed=answer)
     except HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
-    except Exception as err:
-        print(f"Other error occurred: {err}")
-    
 
 bot.run(os.getenv('TOKEN')) # run the bot with the token
